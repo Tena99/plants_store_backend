118157e7fab63213806e762d4e56ea42608891d6
Anastasiia Volvis <stacyvolvis@gmail.com>
2024-04-02 15:43:23 +0200

    Add products route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const users_route = require("./users_route");
+const products_route = require("./products_route");
 
 const route = Router();
 
 route.use("/users", users_route);
+route.use("/products", products_route);
 
 route.get("/", async (request, response) => {
   response.send("Welcome!");
---
+++ routes/products_route.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const connect = require("../lib/connect");
+const Product = require("../modules/products");
+const route = Router();
+
+route.get("/", async (request, response) => {
+  await connect();
+
+  const result = await Product.find({});
+  response.json({ result });
+});
+
+route.get("/:productId", async (request, response) => {
+  await connect();
+
+  const { productId } = request.params;
+  const result = await Product.findOne({ _id: productId });
+
+  response.json(result);
+});
+
+module.exports = route;
