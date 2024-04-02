ab71a85c98f9cc3279b81f099ccbe4db57068026
Anastasiia Volvis <stacyvolvis@gmail.com>
2024-04-02 12:09:37 +0200

    Init

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => {
+  response.send("Welcome!");
+});
+
+app.get("*", (request, response) => {
+  response.status(404).json({ message: "route not defined" });
+});
+
+const server = app.listen(port, () =>
+  console.log(`Express app listening on port ${port}!`)
+);
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
