b77afe052f62ad8bbf1e610e1fa015fcf3a28523
Anastasiia Volvis <stacyvolvis@gmail.com>
2024-04-09 14:23:42 +0200

    Add images

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
+app.use("/images", express.static("assets/images"));
 
 app.get("*", (request, response) => {
   response
