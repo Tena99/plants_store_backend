1bf78eb276920e45040b60c276ccda57be1c251a
Stacy Volvis <strelka1515@gmail.com>
2024-11-16 12:56:29 +0100

    clean-up

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const routes = require("./routes");
+const users_route = require("./routes/users_route");
+const products_route = require("./routes/products_route");
+const auth_route = require("./routes/auth_route");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,10 +11,17 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", routes);
 app.use("/images", express.static("assets/images"));
 
-app.get("*", (request, response) => {
+app.get("/", async (request, response) => {
+  response.send("Welcome to the Plantopia server!");
+});
+
+app.use("/users", users_route);
+app.use("/products", products_route);
+app.use("/auth", auth_route);
+
+app.get("*", (_, response) => {
   response
     .status(404)
     .json({ message: "The route does not exist. Please try another one." });
@@ -22,5 +31,4 @@ const server = app.listen(port, () =>
   console.log(`Express app listening on port ${port}!`)
 );
 
-server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
